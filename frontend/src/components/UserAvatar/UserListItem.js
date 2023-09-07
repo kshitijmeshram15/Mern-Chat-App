2a542e1d13d587079629578ada8d352afc500d65
kshitijmeshram15 <kshitijmeshram15@gmail.com>
2023-09-07 22:17:42 +0530

    chatstate

--- frontend/src/components/UserAvatar/UserListItem.js
+++ frontend/src/components/UserAvatar/UserListItem.js
@@ -1,6 +1,6 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Text } from "@chakra-ui/layout";
-import { ChatState } from "../../Context/ChatProvider";
+//import { ChatState } from "../../Context/ChatProvider";
 
 import React from 'react'
 
