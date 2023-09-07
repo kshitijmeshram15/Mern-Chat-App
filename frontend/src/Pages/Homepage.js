47a4fb1be89d4bbebbd991f62d45eba482ba1585
kshitijmeshram15 <kshitijmeshram15@gmail.com>
2023-09-07 20:51:19 +0530

    my commit

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import{Box, Container,Text,Tab,TabList,TabPanel,TabPanels,Tabs} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+const Homepage = () => {
+
+  const history=useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+
+  return (
+    <Container maxW='xl' centerContent>
+      <Box
+        display='flex'
+        justifyContent='center'
+        p={3}
+        bg={'white'}
+        w="50%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text textAlign="center" fontSize="4xl" fontFamily="Work sans">Talk-A-Tive</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant='soft-rounded'>
+          <TabList marginBottom="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel><Login/></TabPanel>
+            <TabPanel><Signup/></TabPanel>
+          </TabPanels>
+        </Tabs>
+        </Box>
+    </Container> 
+  )
+};
+
+export default Homepage;
---
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,73 @@
+import { ViewIcon } from "@chakra-ui/icons";
+import { Button,IconButton } from "@chakra-ui/button";
+import {useDisclosure} from "@chakra-ui/hooks";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+ 
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import React from 'react'
+
+const ProfileModal = ({ user, children }) => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+    return (
+        <>
+          {children ? (
+            <span onClick={onOpen}>{children}</span>
+          ) : (
+            <IconButton display={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+          )}
+          <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent h="410px">
+              <ModalHeader
+                fontSize="40px"
+                fontFamily="Work sans"
+                display="flex"
+                textAlign="center"
+              >
+                {user.name}
+              </ModalHeader>
+              <ModalCloseButton />
+              <ModalBody
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src={user.pic}
+                  alt={user.name}
+                  mx="auto"
+                />
+                <Text
+                  fontSize={{ base: "28px", md: "30px" }}
+                  fontFamily="Work sans"
+                  textAlign="center"
+                >
+                  Email: {user.email}
+                </Text>
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={onClose}>Close</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      );
+  
+};
+
+export default ProfileModal;  
